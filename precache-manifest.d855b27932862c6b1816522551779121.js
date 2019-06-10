8412ad8f5d22e1a6a1875feb8f409b2b124ce52e
Andrii <gaarasandy@gmail.com>
2019-06-10 21:20:50 +0300

    Updates

---
+++ precache-manifest.d855b27932862c6b1816522551779121.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e8d4dfe9bbd6da65788d683bd6eb1ee",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "42ba43eb4c19b12ed231",
+    "url": "/react_static-list-of-posts/static/css/main.bb30e74a.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react_static-list-of-posts/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "42ba43eb4c19b12ed231",
+    "url": "/react_static-list-of-posts/static/js/main.a6daebc2.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
